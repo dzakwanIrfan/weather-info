c70242574933d51af39175df38324eeca93e94fb
Dzakwan Irfan <lexdani368@gmail.com>
2025-01-06 15:55:09 +0700

    fix: update BASE_URL to use HTTPS for weather API

--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = "d30749b830ac4992988142537243112";
-const BASE_URL = "http://api.weatherapi.com/v1";
+const BASE_URL = "https://api.weatherapi.com/v1";
 
 export const getWeather = async (city: string) => {
     const response = await axios.get(`${BASE_URL}/current.json`, {
